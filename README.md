710b4e92cf12199c7c11ced2905d3818768b337d
liaraSilva <liara.nascimento@alunoescolassociais.org.br>
2024-08-08 12:48:38 -0300

    adiciona arquivo controller.js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Longin_Card
+
+Primeiro tivemos uma aula esplicativa com o professor leonardo esplicando oque cada codigo era e oq ele 
+fazian splise
--- controller.js
+++ controller.js
@@ -10,3 +10,38 @@ function acessar(){
         window.location.href = 'cadastro.html';
     }
 }
+// FUNÇÃO DE CRIAÇÃO DE ARRAY PAR ARMAZENAMENTO DE NOMES
+
+var dadosLista = [];
+
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+         console.log(dadosLista);
+         criaLista();
+         document.getElementById('nomeUser').value = "";
+        } else{
+            alert("Favor, informar um nome para cadastro");
+        }
+    }
+//FUNÇÃO DECRIAÇÃO DE LISTA
+function criaLista(){
+    let tabela = document.getElementById("tabela"). innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+    for(let i = 0; i <= (dadosLista.length - 1); i++){
+        tabela += "<tr><td>" + dadosLista[i] +"</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button> </td></tr>"; 
+        document.getElementById("tabela"). innerHTML = tabela;
+    }
+}
+//Função para excluir Nome de lista 
+
+function excluir(i){
+    dadosLista.splice((i - 1), 1);
+    document.getElementById('tabela').deleteRow(i);
+}
+
+//FUNÇÃO PARA EDITAR NOME DA LISTA
+function editar(i){
+    document.getElementById("nomeUser").value = dadosLista [(i - 1)];
+    dadosLista.splise(dadosLista[(i - 1)], 1)
+}
